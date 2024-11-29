69e6b8a86b7c75f934c856d560d0355c12d7a3fc
Damian Borowiec <dborowiec10@gmail.com>
2024-11-29 12:44:54 +0000

    Add sources

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,29 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+function Text() {
+  return (
+    <span className='text-white'>/home/user/damian</span>
+  ) 
+}
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="scanlines"></div>
+      <div className="scanline"></div>
+      <div className="flicker"></div>
+      <div className="body p-2">
+
+
+
+
+
+
+      <Text />
+
+      </div>
     </div>
   );
 }
